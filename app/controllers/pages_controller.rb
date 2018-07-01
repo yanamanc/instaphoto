1733e132749a0875b7d323970e943240836941c6
Yana Mancevich <yaninamantsevich@mail.ru>
2018-07-01 23:53:03 +0200

    Создана ассоциация user и photo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   def landing
+  	if user_signed_in?
+  		redirect_to "/photos"
+  	end
   end
+  
   def about
   end
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,7 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
-
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :owner, only: [ :edit, :update, :destroy]
   def index
     @photos = Photo.all
   end
@@ -12,7 +12,7 @@ class PhotosController < ApplicationController
 
 
   def new
-    @photo = Photo.new
+    @photo = current_user.photos.build
   end
 
 
@@ -21,42 +21,40 @@ class PhotosController < ApplicationController
 
 
   def create
-    @photo = Photo.new(photo_params)
-
-
+    @photo = current_user.photos.build
       if @photo.save
        redirect_to @photo, notice: 'Photo was successfully created.'
       else
         render :new
       end
-
   end
 
 
   def update
-
       if @photo.update(photo_params)
-        edirect_to @photo, notice: 'Photo was successfully updated.'
-      
+        redirect_to @photo, notice: 'Photo was successfully updated.'
       else
        render :edit      
-
     end
   end
 
 
   def destroy
     @photo.destroy
-     redirect_to photos_url, notice: 'Photo was successfully destroyed.'
+    redirect_to photos_url, notice: 'Photo was successfully destroyed.'
   end
 
   private
-   
     def set_photo
       @photo = Photo.find(params[:id])
     end
 
     def photo_params
       params.require(:photo).permit(:description)
+    end
+
+    def owner
+      @photo=current_user.photos.find_by(id: params[:id])
+      redirect_to photos_path, notice: "У вас нет разрешения на изменение этой фотографии" if @photo.nil?
     end
 end
